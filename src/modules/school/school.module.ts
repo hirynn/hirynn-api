a6cb65d6ae303516a519f2bf7cbfe31083ac4836
AnjanGhimire7 <anjanghimire876@gmail.com>
2025-11-30 21:02:10 +0545

    fix: uploading resume for Anonymous

--- src/modules/school/school.module.ts
+++ src/modules/school/school.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { SchoolService } from './school.service';
 import { SchoolController } from './school.controller';
 import { PrismaService } from '../../database/prisma.service';
-
+import { CloudinaryService } from '../cloudinary/cloudinary.service';
 @Module({
   controllers: [SchoolController],
-  providers: [SchoolService, PrismaService],
+  providers: [SchoolService, PrismaService, CloudinaryService],
   exports: [SchoolService],
 })
 export class SchoolModule {}
